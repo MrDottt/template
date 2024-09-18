5db3bf8a92f1c290c8af5326da7d7689905cf9de
Mr Dott <50643409+MrDottt@users.noreply.github.com>
2024-09-18 06:55:08 +0600

    Update insta_llgo.sh

--- insta_llgo.sh
+++ insta_llgo.sh
@@ -2,12 +2,13 @@
 
 # Download the Go tarball
 GO_VERSION="1.21.1"
-wget https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
 
 # Extract the tarball to /usr/local
-sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.1.linux-amd64.tar.gz
 
 # Install nano if not already installed
+sudo apt update -y
 sudo apt install -y nano
 
 # Add Go binary to PATH in ~/.bashrc
